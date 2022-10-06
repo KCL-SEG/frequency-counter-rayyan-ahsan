708a7fee5d77a5e34badbcda107b7f0c8c28c1ce
rayyan ahsan <raygraphicsreal@gmail.com>
2022-10-07 00:08:52 +0100

    frequencies.py fills all test criteria

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,10 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    items_str = [str(x) for x in items]
+    for i in range(len(items_str)):
+        if str(items_str[i]) in frequencies.keys():
+            frequencies[items_str[i]] = frequencies[items_str[i]] + 1
+        else:
+            frequencies[items_str[i]] = 1
     return frequencies
